d40591055e58b8b66f85a8685680f551aab72fe3
Kieran Quinn <kieran.quinn@hotmail.co.uk>
2013-04-20 00:08:32 +0100

    update

--- Scripts/dwm-status.sh
+++ Scripts/dwm-status.sh
@@ -1,50 +1,42 @@
 #!/bin/sh
 
+icons=("Ñ" "Î" "¨" "ê" "í" "È")
+
 getCPU() {
-    cpu="$(eval $(awk '/^cpu /{print "previdle=" $5 "; prevtotal=" $2+$3+$4+$5 }' /proc/stat); sleep 0.4; eval $(awk '/^cpu /{print "idle=" $5 "; total=" $2+$3+$4+$5 }' /proc/stat); intervaltotal=$((total-${prevtotal:-0})); echo "$((100*((intervaltotal)-($idle-${previdle:-0}) )/(intervaltotal)))")"    
-    echo -e "\x0AÑ\x01${cpu}%"
+    cpu="$(eval $(awk '/^cpu /{print "previdle="$5"; prevtotal="$2+$3+$4+$5}' /proc/stat);sleep 0.4;eval $(awk '/^cpu /{print "idle="$5"; total="$2+$3+$4+$5}' /proc/stat);intervaltotal=$((total-${prevtotal:-0}));echo -ne "$((100*((intervaltotal)-($idle-${previdle:-0}))/(intervaltotal)))")"    
+    echo -ne "\x06${icons[0]}\x01${cpu}%"
 }
 
 getRAM() {
     mem="$(free -m | awk '/-\/+/ {print $3}')"
-    echo -e "\x0AÎ\x01 ${mem}MB"
+    echo -ne "\x06${icons[1]}\x01 ${mem}MB"
 }
 
 getHDD() {
-    root="$(df | awk '/dev\/sda3/ {print $5}')"
-    home="$(df | awk '/dev\/sda4/ {print $5}')"
-    echo -e "\x0A¨\x01 ${root} ${home}"
-}
-
-getUpdates() {
-    echo -e "$(pacman -Qu)"
+    hdd="$(df | awk '/dev\/sda3/ {print $5}') $(df | awk '/dev\/sda4/ {print $5}')"
+    echo -ne "\x06${icons[2]}\x01 ${hdd}"
 }
 
 getMusic() {
-    song="$(ncmpcpp --now-playing '{{{{%a\x0A-\x01}%t}}|{%f}}' | head -c 75)"
-    if [ ! $song ]; then
-        echo -e "\x0Aê\x01 no music"
+    msc="$(ncmpcpp --now-playing '{%a - %t}|{%f}')"
+    if [ ! $msc ]; then
+        echo -ne "\x06${icons[3]}\x01 no music"
     else
-        echo -e "\x0Aê\x01 ${song}" 
+        echo -ne "\x06${icons[3]}\x01 ${msc}" 
     fi
 }
 
 getVolume() {
-    status="$(amixer get Master | awk '/Front Left:/ {print $7}' | tr -dc "a-z")"
-    volume="$(amixer get Master | awk '/Front Left:/ {print $5}' | tr -dc "0-9")"
-    if [ $status = "on" ]; then
-        echo -e "\x0Aí\x01 ${volume}%"
-    else
-        echo -e "\x0Aí\x05 muted"
-    fi
+    vol="$(amixer get PCM | awk '/Front Left:/ {print $5}' | tr -dc '0-9')"
+    echo -ne "\x06${icons[4]}\x01 ${vol}%"
 }
 
-getDate() {
-    datetime="$(date '+%H:%M')"
-    echo -e "\x0AÈ\x01  ${datetime}"
+getTime() {
+    tme="$(date '+ %H:%M')"
+    echo -ne "\x06${icons[5]}\x01 ${tme}"
 }
 
 while true; do
-    xsetroot -name "$(getCPU) $(getRAM) $(getHDD) $(getMusic) $(getVolume) $(getDate)"
+    xsetroot -name "$(getCPU) $(getRAM) $(getHDD) $(getMusic) $(getVolume) $(getTime)"
     sleep 2
 done
